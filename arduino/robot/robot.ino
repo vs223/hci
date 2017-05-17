a082d1469c11fd27f7df1644f088d1578c5e1559
vs223 <vs223@naver.com>
2017-05-17 20:28:22 +0900

    arduino code done

--- arduino/robot/robot.ino
+++ arduino/robot/robot.ino
@@ -7,20 +7,44 @@ int armFlag = 0;
 int goalPos = 40;
 int fsrValue =0;
 
+int ledPin = 11;
+int potValue;
+int ledValue;
+
+int photoValue;
+int oldPhotoValue = 0;
 
 
 void setup() {
   // put your setup code here, to run once:
-  arm.attach(5);
+  arm.attach(4);
+  pinMode(ledPin, OUTPUT);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
   fsrValue = analogRead(A4);
   if(fsrValue > 128){
-    armFlag = 1;
-  }    
+    armFlag = 1;    
+  }
+
+  potValue = analogRead(A0);
+  ledValue = map(potValue, 0, 1023, 0, 255);
+  analogWrite(ledPin, ledValue);
+
+   
+  photoValue = analogRead(A1);
+  if(photoValue < 350 && oldPhotoValue >= 350){
+    for(int i =0; i <6; i++){
+      analogWrite(ledPin, 255);
+      delay(100);
+      analogWrite(ledPin, 0);
+      delay(100);
+    }
+  }
+  oldPhotoValue = photoValue;
+  
   if(pos == 130){
     goalPos=40;
     armFlag = 0;
@@ -35,6 +59,4 @@ void loop() {
 
   arm.write(pos);
   delay(15);
-  
-
 }
